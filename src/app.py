e26e909b0f6c824fe416f1d3d3ac424418f0a732
Jordan Sosnowski <jjs0029@auburn.edu>
2020-07-18 13:50:36 -0500

    Added ability to convert to and from ascii/hex

--- src/app.py
+++ src/app.py
@@ -1,10 +1,55 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html', ascii_text='', hex_text='', x='')
+
+
+@app.route('/', methods=['POST'])
+def encrypt():
+    conversion = ''
+    hex_text = ''
+    ascii_text = ''
+    if 'ascii' in request.form:
+        conversion = 'ascii'
+        text = request.form[f'{conversion}_text']
+        ascii_text = text
+    elif 'hex' in request.form:
+        conversion = 'hex'
+        text = request.form[f'{conversion}_text']
+        hex_text = text
+
+    output = globals()[f"{conversion}_conversion"](request.form, text)
+    # output = hex_conversion(request.form, text)
+    if hex_text == '':
+        hex_text = output
+    elif ascii_text == '':
+        ascii_text = output
+
+    return render_template('index.html', ascii_text=ascii_text, hex_text=hex_text, x='test')
+
+# TODO: cant handle newline characters
+
+
+def hex_conversion(form, text):
+    text = text.split(' ')
+    output = ''
+    for x in text:
+        try:
+            output += chr(int(x, 16))
+        except ValueError:
+            continue
+    return output
+
+
+def ascii_conversion(form, text):
+    output = ''
+    for x in text:
+        output += hex(ord(x)) + " "
+    return output
+
 
 if __name__ == '__main__':
     app.run()
